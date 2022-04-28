04949374d589382c7a3f8aee1a560fb74f4175f7
Henry Miller <mail.henrym+git@gmail.com>
2022-04-28 16:33:06 -0400

    added edit profile functions and updated buttons, no working functionality yet

--- client/src/components/MoreProfileInfo.js
+++ client/src/components/MoreProfileInfo.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../style/moreprofileinfo.css';
 import location from '../img/location_icon.png';
 import userPhoto from '../img/user-icon.png'
@@ -7,39 +7,45 @@ import axios from 'axios';
 let unmatchUrl = 'http://localhost:4000/unmatch';
 let userUrl = 'http://localhost:4000/user';
 
-function MoreProfileInfo ({signedIn,showMoreProfileInfo, setShowMoreProfileInfo, profile, setUser, nameLength,locationLength,handleLogOut}) {
-   let [clickedLogOut,setClickedLogOut] = useState(false);
-   let [clickedEdit,setClickedEdit] = useState(false);
+function MoreProfileInfo({ signedIn, showMoreProfileInfo, setShowMoreProfileInfo, profile, setUser, nameLength, locationLength, handleLogOut }) {
+    let [clickedLogOut, setClickedLogOut] = useState(false);
+    let [clickedEdit, setClickedEdit] = useState(false);
 
-    function handleCloseProfile(){
+    function handleCloseProfile() {
         setShowMoreProfileInfo(!showMoreProfileInfo)
     }
 
 
-    const [username,setUsername] = useState(signedIn?profile.username:null)
-    const [firstName, setFirstName] = useState(signedIn?profile.first_name:null)
-    const [lastName, setLastName] = useState(signedIn?profile.last_name:null)
+    const [username, setUsername] = useState(signedIn ? profile.username : null)
+    const [fullName, setFullName] = useState(signedIn ? profile.name : null)
+    const [location, setLocation] = useState(signedIn ? profile.location : null)
+    const [pronouns, setPronouns] = useState(signedIn ? profile.pronouns : null)
 
     function handleSubmitChanges(e) {
         e.preventDefault();
         if (username === '') {
             setUsername(profile.username)
         }
-    
-        if (firstName === '') {
-            setFirstName(profile.first_name)
+
+        if (fullName === '') {
+            setFullName(profile.name)
         }
-    
-        if (lastName === '') {
-            setLastName(profile.last_name)
+
+        if (location === '') {
+            setLocation(profile.location)
         }
-    
+        
+        if (pronouns === '') {
+            setPronouns(profile.pronouns)
+        }
+
         let updatedDetails = {
-            "first_name": firstName,
-            "last_name": lastName,
+            "name": fullName,
+            location,
+            pronouns,
             username
         }
-    
+
         axios.patch('/users/' + profile.id, updatedDetails)
             .then(r => {
                 setUser(r.data)
@@ -57,27 +63,34 @@ function MoreProfileInfo ({signedIn,showMoreProfileInfo, setShowMoreProfileInfo,
             });
     }
 
-    let buttonsToDisplay = <React.Fragment>
-                                {clickedLogOut?<button className='close-button'>ARE YOU SURE?</button>:<button className='close-button' onClick={handleCloseProfile}>CLOSE PROFILE</button>}
-                                {clickedLogOut?<button className='close-button' onClick={handleLogOut}>CONFIRM LOG OUT</button>:<button className='close-button' onClick={()=>{setClickedEdit(!clickedEdit)}}>EDIT PROFILE</button>}
-                                {clickedLogOut?<button className='close-button' onClick={()=>{setClickedLogOut(false)}}>CANCEL</button>:<button className='close-button' onClick={()=>{setClickedLogOut(true)}}>LOG OUT</button>}
-                            </React.Fragment>
-    
+    let LogOutbuttonsToDisplay = <React.Fragment>
+        {clickedLogOut ? <button className='close-button'>ARE YOU SURE?</button> : <button className='close-button' onClick={handleCloseProfile}>CLOSE PROFILE</button>}
+        {clickedLogOut ? <button className='close-button' onClick={handleLogOut}>CONFIRM LOG OUT</button> : <button className='close-button' onClick={() => { setClickedEdit(!clickedEdit) }}>EDIT PROFILE</button>}
+        {clickedLogOut ? <button className='close-button' onClick={() => { setClickedLogOut(false) }}>CANCEL</button> : <button className='close-button' onClick={() => { setClickedLogOut(true) }}>LOG OUT</button>}
+    </React.Fragment>
+
+    let editButtonsToDisplay = <React.Fragment>
+        <button className='close-button' onClick={handleSubmitChanges}>CONFIRM</button> 
+        <button className='close-button' onClick={() => { setClickedEdit(false) }}>CANCEL</button> 
+        </React.Fragment>
+
+    let buttonsToDisplay = clickedEdit?editButtonsToDisplay:LogOutbuttonsToDisplay
+
 
     return (
         <div className='moreProfileInfo-cardContainer'>
-            <div className={profile.name==='Jonathan'?'moreuser-info-card':'moreprofile-info-card'}>
-            {profile.name==='Jonathan'?<img className='moreuser-info-img' src={userPhoto}/>:<img className='moreprofile-info-img' src={profile.image}/>}
+            <div className={profile.name === 'Jonathan' ? 'moreuser-info-card' : 'moreprofile-info-card'}>
+                <img className='moreprofile-info-img' src={profile.image} />
                 <div className='info-box'>
-                    <h1 className={nameLength > 10?'card-title-long':'card-title'}>{profile.name}</h1>
-                    <h3 className={locationLength > 15? 'card-location-long':'card-location'}><img className ='location-icon-here' src={location}/>{profile.location}</h3>
+                    <h1 className={nameLength > 10 ? 'card-title-long' : 'card-title'}>{profile.name}</h1>
+                    <h3 className={locationLength > 15 ? 'card-location-long' : 'card-location'}><img className='location-icon-here' src={location} />{profile.location}</h3>
                     <h3 className='card-subtitle'>{profile.pronouns} - {profile.age} years dead</h3>
                     <hr></hr>
                     <p className='card-text'>{profile.description}</p>
                     {buttonsToDisplay}
                 </div>
             </div>
-      </div>
+        </div>
     )
 }
 
