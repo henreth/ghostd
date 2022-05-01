9b82e7b3a34c67bd9cb984ecc59c9ed9b84d3e30
Henry Miller <mail.henrym+git@gmail.com>
2022-04-30 20:24:49 -0400

    build

--- client/src/components/ProfileCard/ProfileCard.js
+++ client/src/components/ProfileCard/ProfileCard.js
@@ -5,25 +5,25 @@ import info from '../../img/info-icon.png';
 import './profilecard.css';
 
 
-export default function ProfileCard({childRefs,index,profile, swiped, outOfFrame, id,handleClickInfoButton}){
+export default function ProfileCard({ childRefs, index, profile, swiped, outOfFrame, id, handleClickInfoButton }) {
   const charImages = require.context('../../img/characters', true);
 
-    let profileDescription = profile.description.split('').length >50 ? profile.description.slice(0,50)+'...':profile.description
+  let profileDescription = profile.description.split('').length > 50 ? profile.description.slice(0, 50) + '...' : profile.description
 
-    return(
-        <TinderCard
-        ref={childRefs[index]}
-        className='swipe'
-        key={profile.name}
-        onSwipe={(dir) => swiped(dir, profile, index,id)}
-        onCardLeftScreen={() => outOfFrame(profile.name, index)}
-      >
-        <div className='card'>
-          <h3 className='name-age'>{profile.name}, <span>{profile.age}</span></h3>
-          <img className='img' src={charImages('./'+profile.image)}/>
-          <div className='description-text'>{profileDescription}</div>
-          <img className='info-icon' onClick={handleClickInfoButton} src={info} />
-        </div>
-      </TinderCard>
-    )
+  return (
+    <TinderCard
+      ref={childRefs[index]}
+      className='swipe'
+      key={profile.name}
+      onSwipe={(dir) => swiped(dir, profile, index, id)}
+      onCardLeftScreen={() => outOfFrame(profile.name, index)}
+    >
+      <div className='card'>
+        <h3 className='name-age'>{profile.name}, <span>{profile.age}</span></h3>
+        <img className='img' src={charImages('./' + profile.image)} />
+        <div className='description-text'>{profileDescription}</div>
+        <img className='info-icon' onClick={handleClickInfoButton} src={info} />
+      </div>
+    </TinderCard>
+  )
 }
